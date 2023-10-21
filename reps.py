df7eb9a5abd6e1fe02ce1fd5c326834cd36e27cd
sfgeekgit <sfgeek@gmail.com>
2023-10-21 12:22:51 -0700

    Start by getting repdigit numbers

---
+++ reps.py
@@ -0,0 +1,15 @@
+
+# function to return a list of all repdigits up to a given length
+def get_repdigits(max_len = 6, use_zero=True):
+    repdigits = []
+    if use_zero:
+        repdigits = [0]
+    for lenn in range(1, max_len+1):
+        ones = 0;
+        for place in range(0, lenn):
+            # make the ones, like 111111
+            ones = ones + 10**place
+        for i in range(1,10):
+            # fill in the list with ones * i
+            repdigits.append(ones * i)
+    return repdigits
