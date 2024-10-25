b41326f69aac4a5e1fada98086025d3daed425a5
root <root@debian-s-1vcpu-2gb-sfo3-01>
2024-10-25 20:03:17 +0000

    refactor, for large terms will need to split up how memeory is stored, working toward that

--- min_reps_dp.py
+++ min_reps_dp.py
@@ -2,17 +2,18 @@ from reps import get_repdigits
 import math
 
 
-# dynamic programing builds up the entire array from 0..target
+# dynamic programing builds up the entire array from 0..ceiling
 # builds array of how many rep digits are needed for EVERY number below
 # so can't skip numbers
 # should not be re-invoked for every sequential number, that will re-do most of the work every time.
 
 
-def calc_sequence(target, repdigits=None):
+def calc_dp_array(ceiling, keep_terms = True, dp=None):
+    
     # this uses dynamic programing dp to store values and build up
     # it calclulates how many are needed for 0, then 1, then 2, etc.
-    # builds all the way up to how many rep_digs are needed for target
-    # so this should be called once, with a big target, or maybe return the dp array
+    # builds all the way up to how many rep_digs are needed for ceiling
+    # so this should be called once, with a big ceiling, or maybe return the dp array
 
     # algo starts with how many "1" to make each number
     ### (will always be that number. It takes 1+1+1+1 to make a 4  therefore  dp[4] = 4 at this stage
@@ -26,39 +27,52 @@ def calc_sequence(target, repdigits=None):
     # parent is the LAST repdigit used,
     # so if parent[15] == 11 then this algo used an 11 to make 15, and 15-11=4 so check parent[4] too see the next one
 
-    save_parent = True
-    
-    if not repdigits:
-        if target == 0:  # Special case for 0
-            max_len = 1
-        else:
-            max_len = math.floor(math.log10(target)) + 1
-        repdigits = get_repdigits(max_len)
+    if keep_terms:
+        save_parent = True
+        parent = [1] * (ceiling + 1)
+        # tried running without parent to see if it's faster.
+        # Answer, a little faster, but not much. Should save memory.
+
+
+    else:
+        save_parent = False
+        parent=[]
+
+    if ceiling == 0:  # Special case for 0
+        max_len = 1
+    else:
+        max_len = math.floor(math.log10(ceiling)) + 1
+    repdigits = get_repdigits(max_len)
         
     ##
-    dp_reps_needed = list(range(target+1)) # start with just the digit "1" Each int will will need that many ones if only using "1"
+    dp_reps_needed = list(range(ceiling+1)) # start with just the digit "1" Each int will will need that many ones if only using "1"
 
-    if save_parent:
-        parent = [1] * (target + 1)
-    # tried running without parent to see if it's faster.
-    # Answer, a little faster, but not much
 
 
     
     for repdigit in repdigits[1:]:  # already init dp for "1"
-        for i in range(repdigit, target + 1):
+        for i in range(repdigit, ceiling + 1):
             #if dp_reps_needed[i] > dp_reps_needed[i - repdigit] + 1:
             if dp_reps_needed[i] >= dp_reps_needed[i - repdigit] + 1: # greedy gives nicer numbers.
                 dp_reps_needed[i] = dp_reps_needed[i - repdigit] + 1
                 if save_parent:
                     parent[i] = repdigit
-            if i % 222_000 == 0:
-                print(f"{i=} \t{repdigit=} {target=} {dp_reps_needed[i]=}")
+            if i % 2_222_000 == 0:
+                print(f"{i=} \t{repdigit=} {ceiling=} {dp_reps_needed[i]=}")
 
                 
-    if dp_reps_needed[target] == float('inf'):
-        return None, []
+    return dp_reps_needed, parent
 
+    
+def calc_sequence(ceiling, dp_reps_needed=None, parent=None, get_terms=True):
+
+        
+    if dp_reps_needed is None or (get_terms and parent is None):        
+        dp_reps_needed, parent = calc_dp_array(ceiling, dp=[123])
+
+
+
+    
     # OK, now we have the list of how many digits are needed for every number!
     # itterate up the list to find the FIRST of each number to build the sequence!
     sequence = []
@@ -86,15 +100,28 @@ def calc_sequence(target, repdigits=None):
 #res = calc_sequence(320, reps)
 #res = calc_sequence(3332216, reps)
 #ceiling = 333
-#ceiling =         32_218
+ceiling =         32_218
 #ceiling =       332_217  # known 7th term
 #ceiling =     3_332_216  # known 8th term
 #ceiling =    33_332_215  # known 9th
-ceiling =   333_332_214  # known 10th term
+#ceiling =   333_332_214  # known 10th term
 #ceiling = 3_333_332_213  # known 11th term
 
+
+#ceiling = 25
+dp, last_terms = calc_dp_array(ceiling)
+#print (dp)
+#print(last_terms)
+#quit()
+
+
+###def calc_sequence(ceiling, dp_reps_needed=None, parent=None, get_terms=True):
+
+sequence, reps_used = calc_sequence(ceiling, dp, last_terms)
 sequence, reps_used = calc_sequence(ceiling)
 print(f"{sequence=}")
+
+
 
 
 for i, term in enumerate(sequence):
@@ -111,42 +138,6 @@ for i, term in enumerate(sequence):
 #ceiling = 2_333_332_213  # This Does Not Fit  in memory (8G ram and 16G swap) store_parent=True
 
 
-
-# old, bad approach, does not make proper use of dynamic programing, this loop starts over every time
-'''
-seq_out = []
-seeking = 1
-seeking_term_n = 7
-
-#reps = get_repdigits(4)
-if seeking <= 2:
-    reps = get_repdigits(2)
-
-for target in range(3333333):        
-    res = calc_min_reps_needed(target, reps)
-    need = res[0]
-    if target > 1111 and target%100==0:
-        print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-        print(f"check target {target=}", flush=True)
-    #used = res[1]
-    #print (f"{target} \tis the sum of {need} repdigits {used}")
-    if need == seeking:
-        print("\n Term Found! \n", target)
-        used = res[1]
-        print (f"{target} \tis the sum of no less than {need} repdigits {used}")
-        seq_out.append(target)
-        print(seq_out, flush=True)
-        if seeking == seeking_term_n:
-            print("Done!")
-            quit()
-        seeking += 1
-        # get new pool of all rep digits up to length n-1
-        reps = get_repdigits(seeking-1)
-        #print(f"now seeking {seeking=} {reps=}")
-    #print(res)
-
-print(seq_out)
-'''
 
 # https://oeis.org/A336748
 # 1, 10, 21, 320, 2219, 32218, 332217, 3332216, 33332215, 333332214, 3333332213 
